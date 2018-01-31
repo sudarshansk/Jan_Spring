44c5d6667549ef7a99d5584f296c44847b55184d
prabhu-durasoft <iamprabhu@yahoo.com>
2018-01-31 14:59:13 +0530

    Day02

--- Day02DB/src/main/java/com/intuit/lab02/service/AccountService.java
+++ Day02DB/src/main/java/com/intuit/lab02/service/AccountService.java
@@ -3,6 +3,8 @@ package com.intuit.lab02.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.intuit.lab02.dao.AccountDao;
 import com.intuit.lab02.dao.StatementDao;
@@ -21,7 +23,7 @@ public class AccountService {
 		accountDao.deposit(accountNumber, amount);
 		statementDao.addStatement(accountNumber, type, amount);
 	}
-	
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void withdraw(int accountNumber,String type, int amount)
 	{
 		accountDao.withdraw(accountNumber, amount);
