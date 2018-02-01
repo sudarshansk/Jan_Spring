4a3e0ac2629bd4ee4ab6289c526049848daefd8c
prabhu-durasoft <iamprabhu@yahoo.com>
2018-02-01 15:14:06 +0530

    Day03

--- Day03JPA/src/main/java/com/intuit/dao/PersonRepository.java
+++ Day03JPA/src/main/java/com/intuit/dao/PersonRepository.java
@@ -3,6 +3,7 @@ package com.intuit.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.intuit.domain.Person;
@@ -10,6 +11,11 @@ import com.intuit.domain.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer>{
 		//id,name,age
+		
+		//@Query("select * from persons where age > ?")
+		@Query("select p from Person p where p.age > :ageParam")
+		List<Person> findAllPersonsWithAgeGreaterThan(int ageParam);
+	
 		Person findOneById(int id);
 		List<Person> findByName(String name);
 		List<Person> findByNameAndAge(String name,int age);
