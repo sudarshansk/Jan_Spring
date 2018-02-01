ff2c715b87e6469e40ce459e622d1afbc4812daf
prabhu-durasoft <iamprabhu@yahoo.com>
2018-02-01 15:07:19 +0530

    Day03

--- Day03JPA/src/main/java/com/intuit/dao/PersonRepository.java
+++ Day03JPA/src/main/java/com/intuit/dao/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.intuit.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,25 @@ import com.intuit.domain.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer>{
-
+		//id,name,age
+		Person findOneById(int id);
+		List<Person> findByName(String name);
+		List<Person> findByNameAndAge(String name,int age);
+		List<Person> findByAge(int age);
+		List<Person> findByIdAndAge(int id,int age);
+		
+		List<Person> findByAgeBetween(int min,int max);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
