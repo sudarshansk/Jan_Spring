edea916e700f210f70f27821a2045d743a35f5fb
prabhu-durasoft <iamprabhu@yahoo.com>
2018-02-02 10:58:14 +0530

    Day04

--- Day04Boot/src/main/java/com/intuit/ExerciseController.java
+++ Day04Boot/src/main/java/com/intuit/ExerciseController.java
@@ -1,5 +1,9 @@
 package com.intuit;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ExerciseController {
 
 	@PostMapping("/process")
-	public String processForm(@RequestParam("firstname") String name,Model model) {
+	public String processForm(@RequestParam("firstname") String name,Model model,
+			HttpSession session, HttpServletRequest request, 
+			HttpServletResponse response) {
 		String message = "Hello " + name;
+		session.setAttribute("cool", "cold in session");
 		model.addAttribute("message",message);
 		return "result";
 	}
